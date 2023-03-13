748ac941584b1ce14c257b4198a0a934aa7606ef
Abijahh <124657902+Abijahh@users.noreply.github.com>
2023-03-13 10:45:46 +0300

    portfolio

---
+++ NavBar/side_bar/Javascript/index.js
@@ -0,0 +1,22 @@
+/*create variables.*/
+let num=20
+if(num<10){
+    // code if condition is true
+    console.log("number is less than 10")
+}else{
+    console.log("number is greater than 10")
+}
+
+let country="Chad"
+let age="18"
+if(country=="Togo"){
+    //code if true
+    if(age>=18){
+        console.log("Accepted")
+    }
+}else{
+    console.log("Not accepted")
+}
+
+
+
